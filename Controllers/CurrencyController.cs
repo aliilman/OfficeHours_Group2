ce2090c94f646699508e22f1d5ba12045bad19cd
Ali İLMAN <aliilman48@gmail.com>
2024-01-04 20:32:07 +0300

    first commit

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OfficeHours_Group2.Controllers
+{
+    [Route("[controller]")]
+    public class CurrencyController : Controller
+    {
+        //seçilen kura göre bilgilerini getiren api
+
+        
+
+
+
+        // girilen değer için conversion yapan
+        // desteklenen currency tipler
+    }
+}
