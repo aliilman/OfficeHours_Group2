b269ff6236e354f44bc750819828b621c468daf5
Ali İLMAN <aliilman48@gmail.com>
2024-01-07 19:01:37 +0300

    add convert and live

---
+++ Dtos/Quote.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeHours_Group2.Dtos
+{
+    public class Quote
+    {
+        public string Currency { get; set; }
+        public double Value { get; set; }
+    }
+}
